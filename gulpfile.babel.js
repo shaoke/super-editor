409866f54b084aced9e3781e4106dbbfe005d80c
Shaoke Xu <shaoxu@tibco.com>
2015-12-28 17:45:21 -0800

    Add basic page of super editor, and also add the automation to generate chrome app

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -14,13 +14,21 @@ const $ = gulpLoadPlugins();
 
 const CONFIG = {
     stackEditTag: "v4.3.11",
-    stackEditSrc: "vendor-src/stackedit"
+    stackEditSrc: "vendor-src/stackedit",
+    dist: "dist",
+    chromeAppSrc: "chrome-app",
+    appName: "superedit",
+    src: "src"
 };
 
 gulp.task('default', ()=> {
 
 });
 
+//===========================================
+// git - submodule
+
+//-------------------------------------------
 // Init and Update **submodule-stackedit** to specific version
 gulp.task('submodule:stackedit', ()=> {
     // init submodule
@@ -31,4 +39,53 @@ gulp.task('submodule:stackedit', ()=> {
     cp.execSync('git checkout ' + CONFIG.stackEditTag, {cwd: CONFIG.stackEditSrc});
 });
 
+//===========================================
+// stackedit tasks
 
+//-------------------------------------------
+// npm install - install nodejs packages
+gulp.task('stackedit:npm:install', ()=>{
+    cp.execSync('npm install', {cwd: CONFIG.stackEditSrc});
+});
+
+//-------------------------------------------
+// bower install - install javascript libraries
+gulp.task('stackedit:bower:install', ()=>{
+    cp.execSync('bower install', {cwd: CONFIG.stackEditSrc});
+});
+
+//-------------------------------------------
+// Build/minify
+gulp.task('stackedit:build', ()=>{
+    cp.execSync('gulp', {cwd: CONFIG.stackEditSrc});
+});
+
+//===========================================
+// Chrome App
+
+//-------------------------------------------
+// clean generate app
+gulp.task('chromapp:clean', ()=>{
+    del(path.join(CONFIG.dist, CONFIG.appName));
+});
+
+//-------------------------------------------
+// copy chrome app
+gulp.task('chromeapp:copy:chrome-app-src', ()=>{
+    gulp.src("**/*", {cwd: CONFIG.chromeAppSrc})
+        .pipe(gulp.dest(path.join(CONFIG.dist, CONFIG.appName)));
+});
+
+//-------------------------------------------
+// copy source
+gulp.task('chromeapp:copy:src', ()=>{
+    gulp.src("**/*", {cwd:CONFIG.src})
+        .pipe(gulp.dest(path.join(CONFIG.dist, CONFIG.appName)));
+});
+
+//-------------------------------------------
+// chrome app task
+gulp.task('chromeapp', ['chromapp:clean', 'chromeapp:copy:chrome-app-src', 'chromeapp:copy:src']);
+
+//===========================================
+//-------------------------------------------
